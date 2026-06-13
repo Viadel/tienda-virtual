bb7d51c20fbce82701cc37e19a890faf3a9d333f
Viadel <v60684909@gmail.com>
2026-06-13 13:57:34 -0500

    Quitar base config para compatibilidad con Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+ // base: '/tienda-virtual/', 
 })
